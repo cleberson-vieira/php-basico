80344135d23c9d12ead179d10d7b455ee5eab294
cleberson-vieira <69166207+cleberson-vieira@users.noreply.github.com>
2025-12-26 17:24:10 -0300

    o

--- a05/04-valore-recebidos2.php
+++ a05/04-valore-recebidos2.php
@@ -12,7 +12,7 @@
         echo "<h2>Valore Recebidos: $v1 e $v2</h2>";
         echo "O valor absoluto de $v2 é " . abs($v2);
         echo "<br/> O valor de $v1 <sup>$v2</sup>  é " . pow($v1, $v2);
-        echo "<br/> A raz de $v1 é " . sqrt($v1);
+        echo "<br/> A raiz de $v1 é " . sqrt($v1);
         echo "<br/>O valor de $v2 arredondado é " . round($v2); //ceil floor
         echo "<br/> A parte inteira de $v2 é " . intval($v2);
         echo "<br/> O avlor de $v1 em moeda é R$ " .number_format($v1, 2, ",", ".");
