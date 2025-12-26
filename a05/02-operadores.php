3b0f27e50a55910ba54c82d5471a94a75d10536c
cleberson-vieira <69166207+cleberson-vieira@users.noreply.github.com>
2025-12-26 14:20:51 -0300

    ajuste

--- a05/02-operadores.php
+++ a05/02-operadores.php
@@ -7,21 +7,21 @@
 </head>
 <body>
     <h2>Operações Aritméticas</h2>
-    <?php 
-        // Ordem de Precedência dos operadores
-        //1º - () -> Parênteses
-        //2º - *, /, % -> Multiplicação, Divisão e Módulo (Resto da Divisão)
-        //3º - +, - -> Adição e Subtração
-        $n1 = 3;
-        $n2 = 2;
-        $m = ($n1 + $n2) / 2;
+        <?php 
+            // Ordem de Precedência dos operadores
+            //1º - () -> Parênteses
+            //2º - *, /, % -> Multiplicação, Divisão e Módulo (Resto da Divisão)
+            //3º - +, - -> Adição e Subtração
+            $n1 = 3;
+            $n2 = 2;
+            $m = ($n1 + $n2) / 2;
 
-        echo "A soma entre $n1 e $n2 é igual a " . ($n1 + $n2) . "<br>";
-        echo "A subtração entre $n1 e $n2 é igual a " . ($n1 - $n2) . "<br>";
-        echo "A multiplicação entre $n1 e $n2 é igual a " . ($n1 * $n2) . "<br>";
-        echo "A divisão entre $n1 e $n2 é igual a " . ($n1 / $n2) . "<br>";
-        echo "O módulo entre $n1 e $n2 é igual a " . ($n1 % $n2) . "<br>";
-        echo "A média entre $n1 e $n2 é igual a " . $m . "<br>";
-    ?>
+            echo "A soma entre $n1 e $n2 é igual a " . ($n1 + $n2) . "<br>";
+            echo "A subtração entre $n1 e $n2 é igual a " . ($n1 - $n2) . "<br>";
+            echo "A multiplicação entre $n1 e $n2 é igual a " . ($n1 * $n2) . "<br>";
+            echo "A divisão entre $n1 e $n2 é igual a " . ($n1 / $n2) . "<br>";
+            echo "O módulo entre $n1 e $n2 é igual a " . ($n1 % $n2) . "<br>";
+            echo "A média entre $n1 e $n2 é igual a " . $m . "<br>";
+        ?>
 </body>
 </html>
